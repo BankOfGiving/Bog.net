fdb462d91fd40c01a94c147dc25dce1386392418
dbxt <david.w.baxter@gmail.com>
2014-06-11 22:03:49 -0700

    Replaced missing files.

---
+++ Bog.Web.Api/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿namespace Bog.Web.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        #region Public Methods and Operators
+
+        public void Delete(int id)
+        {
+        }
+
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        #endregion
+
+        // DELETE api/values/5
+    }
+}
